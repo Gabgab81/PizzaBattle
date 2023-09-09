7c3c21e65008a5779246442ce94661bc54322870
gabriel roux <rouxgab81@gmail.com>
2023-09-09 16:17:35 -0400

    battle System part 1 done bis

---
+++ content/actions.js
@@ -0,0 +1,11 @@
+window.Actions = {
+  damage1: {
+    name: "Whomp!",
+    // type:
+    success: [
+      { type: "textMessage", text: "{CASTER} uses Womp!" },
+      // { type: "animation", animation: 'WillBeDefinedHere' },
+      // { type: "stateChange", damage: 10 },
+    ]
+  }
+}
